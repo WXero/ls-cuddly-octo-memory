84040c60c8744c02d12dc64292729227a2a3d9b4
xero <xero45giri.me@gmail.com>
2025-03-02 23:51:48 +0100

    1.0

---
+++ fivem/fxmanifest.lua
@@ -0,0 +1,11 @@
+this_is_a_map 'yes'
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+
+files {
+    'stream/downtown_vinewood/los_santos_freeway.ymt',
+}
+
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'los_santos_freeway.ymt' --VINEWOOD SCENARIOS
