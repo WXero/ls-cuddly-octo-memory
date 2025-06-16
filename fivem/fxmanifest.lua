924cf489bd0babea1fb430607f5e9769913041d7
xero <101067357+WXero@users.noreply.github.com>
2025-06-16 22:27:58 +0200

    Fix incorrect path in fxmanifest

--- fivem/fxmanifest.lua
+++ fivem/fxmanifest.lua
@@ -8,4 +8,4 @@ files {
     'stream/downtown_vinewood/los_santos_freeway.ymt',
 }
 
-data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'los_santos_freeway.ymt' --VINEWOOD SCENARIOS
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'stream/downtown_vinewood/los_santos_freeway.ymt' --VINEWOOD SCENARIOS
